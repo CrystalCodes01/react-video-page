b5f84d2ded8e08935053c8df4b3f080170ba95c6
CrystalSchurko <cschurko@banyanhill.com>
2021-12-20 16:02:22 -0500

    video detail

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -7,15 +7,15 @@ class SearchBar extends Component {
     this.setState({ term: event.target.value });
   };
 
-  onFormSubmit = (event) => {
+  onTermSubmit = (event) => {
     event.preventDefault();
-    this.props.onFormSubmit(this.state.term);
+    this.props.onTermSubmit(this.state.term);
   };
 
   render() {
     return (
       <div className="search-bar ui segment">
-        <form onSubmit={this.onFormSubmit} className="ui form">
+        <form onSubmit={this.onTermSubmit} className="ui form">
             <div className="field">
                 <label>Video Search</label>
                 <input
--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,12 +1,19 @@
 import React from "react";
+import './VideoItem.css';
 
-const VideoItem = ({ video }) => {
+const VideoItem = ({ video, onVideoSelect }) => {
   return (
-    <div className="item">
-      <img alt={video.snippet.title} src={video.snippet.thumbnails.medium.url} />
-      {video.snippet.title}
+    <div onClick={() => onVideoSelect(video)} className="video-item item">
+      <img
+        className="ui image"
+        alt={video.snippet.title}
+        src={video.snippet.thumbnails.medium.url}
+      />
+      <div className="content">
+        <div className="header">{video.snippet.title}</div>
+      </div>
     </div>
-  )
+  );
 };
 
 export default VideoItem;
