485e5b7a31443b9a9c8d6137502a498e78153134
CrystalSchurko <cschurko@banyanhill.com>
2021-12-20 16:09:45 -0500

    call description

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -5,8 +5,9 @@ const VideoDetail = ({ video }) => {
     return <div>Loading...</div>;
   }
   return (
-    <div className="">
-      {video.snippet.title}
+    <div className="ui segment">
+      <h4 className="ui header">{video.snippet.title}</h4>
+      <p>{video.snippet.description}</p>
     </div>
   )
 };
