19dd9acf1b272275bd6f23f403612986c7e0250b
CrystalSchurko <cschurko@banyanhill.com>
2021-12-21 16:59:33 -0500

    grid system added

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,45 @@
 import React, { Component } from "react";
 import SearchBar from "./SearchBar";
 import VideoList from "./VideoList";
-import VideoDetail from './VideoDetail';
-import youtube from '../apis/youtube';
+import VideoDetail from "./VideoDetail";
+import youtube from "../apis/youtube";
 
 class App extends Component {
   state = { videos: [], selectedVideo: null };
 
   onTermSubmit = async (term) => {
-    const response = await youtube.get('/search', {
+    const response = await youtube.get("/search", {
       params: {
-        q: term
-      }
+        q: term,
+      },
     });
 
     this.setState({ videos: response.data.items });
   };
 
   onVideoSelect = (video) => {
-    this.setState({ selectedVideo: video })
+    this.setState({ selectedVideo: video });
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onTermSubmit={this.onTermSubmit} />
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
